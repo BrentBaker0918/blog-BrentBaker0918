ad9318dbd30db22106661e334013213dc8247122
Jonathan Baker <bakerb_2006@hotmail.com>
2020-01-18 21:20:20 -0500

    travis

--- launch_blog/settings.py
+++ launch_blog/settings.py
@@ -75,13 +75,13 @@ WSGI_APPLICATION = 'launch_blog.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    # }
     'default': {
-         dj_database_url.config(default='postgres://postgres:postgres@127.0.0.1:5432/launch_blog')
-     }
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+    'default': {
+        dj_database_url.config(default='postgres://postgres:postgres@127.0.0.1:5432/launch_blog')
+    }
 }
 
 
