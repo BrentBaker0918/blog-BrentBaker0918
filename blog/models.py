5044e9d1a4f337493b86a2daa571181cbc1a6b73
Jonathan Baker <bakerb_2006@hotmail.com>
2020-01-20 21:20:31 -0500

    update

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 from django.conf import settings # imports Django's loaded settings
 from django.db import models
+from pytz import timezone
 
 # Create your models here.
 class Topic(models.Model):
