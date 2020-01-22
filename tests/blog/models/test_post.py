417ef2558113ad7c3cf6875ccd454406d2c2d6cf
Jonathan Baker <bakerb_2006@hotmail.com>
2020-01-21 21:01:24 -0500

    update

--- tests/blog/models/test_post.py
+++ tests/blog/models/test_post.py
@@ -1,6 +1,6 @@
 # tests/blog/models/test_post.py
 
-from model_bakery import mommy
+from model_bakery import baker
 import pytest
 import datetime as dt
 from freezegun import freeze_time
@@ -12,22 +12,22 @@ pytestmark = pytest.mark.django_db
 @freeze_time(dt.datetime(2030, 6, 1, 12), tz_offset=0)  # Replaces now()
 def test_publish_sets_published_to_current_datetime():
     # Create a new post, and ensure no published datetime is set
-    post = mommy.make('blog.Post', published=None)
+    post = baker.make('blog.Post', published=None)
     post.publish()
 
     # Set the timezone to UTC (to match tz_offset=0)
     assert post.published == dt.datetime(2030, 6, 1, 12, tzinfo=dt.timezone.utc)
 
 def test_publish_sets_status_to_published():
-    post = mommy.make('blog.Post', status=Post.DRAFT)
+    post = baker.make('blog.Post', status=Post.DRAFT)
     post.publish()
     assert post.status == Post.PUBLISHED
 
 def test_published_posts_only_returns_those_with_published_status():
     # Create a published Post by setting the status to "published"
-    published = mommy.make('blog.Post', status=Post.PUBLISHED)
+    published = baker.make('blog.Post', status=Post.PUBLISHED)
     # Create a draft Post
-    mommy.make('blog.Post', status=Post.DRAFT)
+    baker.make('blog.Post', status=Post.DRAFT)
 
     # We expect only the "publised" object to be returned
     expected = [published]
