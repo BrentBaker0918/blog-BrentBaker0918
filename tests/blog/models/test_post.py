a4c6759d2dae6edb03a26251e2994dd83b95c99b
Jonathan Baker <bakerb_2006@hotmail.com>
2020-01-21 20:26:00 -0500

    back to mommy - then use migrate from mommy stuff

--- tests/blog/models/test_post.py
+++ tests/blog/models/test_post.py
@@ -1,6 +1,6 @@
 # tests/blog/models/test_post.py
-from django.test import TestCase
-from model_bakery import baker
+
+from model_bakery import mommy
 import pytest
 import datetime as dt
 from freezegun import freeze_time
@@ -10,24 +10,24 @@ from django.utils import timezone
 # Mark this test module as requiring the database
 pytestmark = pytest.mark.django_db
 @freeze_time(dt.datetime(2030, 6, 1, 12), tz_offset=0)  # Replaces now()
-def test_publish_sets_published_to_current_datetime(TestCase):
+def test_publish_sets_published_to_current_datetime():
     # Create a new post, and ensure no published datetime is set
-    post = baker.make('blog.Post', published=None)
+    post = mommy.make('blog.Post', published=None)
     post.publish()
 
     # Set the timezone to UTC (to match tz_offset=0)
     assert post.published == dt.datetime(2030, 6, 1, 12, tzinfo=dt.timezone.utc)
 
 def test_publish_sets_status_to_published():
-    post = baker.make('blog.Post')
+    post = mommy.make('blog.Post', status=Post.DRAFT)
     post.publish()
     assert post.status == Post.PUBLISHED
 
 def test_published_posts_only_returns_those_with_published_status():
     # Create a published Post by setting the status to "published"
-    published = baker.make('blog.Post', status=Post.PUBLISHED)
+    published = mommy.make('blog.Post', status=Post.PUBLISHED)
     # Create a draft Post
-    baker.make('blog.Post')
+    mommy.make('blog.Post', status=Post.DRAFT)
 
     # We expect only the "publised" object to be returned
     expected = [published]
