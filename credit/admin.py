eebf99f35b477d4f06e112c457932e4747638cfd
Jonathan Baker <bakerb_2006@hotmail.com>
2020-02-02 17:40:17 -0500

    update

--- credit/admin.py
+++ credit/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from . import CreditRating
+from . import models
 # Register your models here.
-@admin.register(models.Model)
-class CreditRating(admin.ModelAdmin):
+@admin.register(models.CreditRating)
+class CreditRatingAdmin(admin.ModelAdmin):
     list_display = (
         'user',
         'score',
