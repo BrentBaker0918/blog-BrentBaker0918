13795d42bb9a9974138505bb997a542943d914df
Jonathan Baker <bakerb_2006@hotmail.com>
2020-04-13 22:40:10 -0400

    just trying things now :<

--- launch_blog/settings.py
+++ launch_blog/settings.py
@@ -128,7 +128,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(os.environ.get('CLOUDCUBE_URL'), 'media')
 
 AWS_LOCATION = 'ws4fv3hxlyxl/public/'
 AWS_STORAGE_BUCKET_NAME = 'cloud-cube'
