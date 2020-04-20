098fdb124fcbaedc140c7e52e5818de5eeab463f
Jonathan Baker <bakerb_2006@hotmail.com>
2020-04-20 18:47:23 -0400

    :(

--- blog/context_processors.py
+++ blog/context_processors.py
@@ -5,7 +5,7 @@ from . import models
 def base_context(request):
     latest_posts = models.Post.objects.published().order_by('-published')[:3]
     authors = models.Post.objects.published().get_authors().order_by('first_name')
-    top_topics = models.Topic.objects.order_by('-blog_posts__count').annotate(Count('blog_posts')).values('id', 'name', 'slug','blog_posts__count')
+    top_topics = models.Topic.objects.order_by('-blog_posts__count').annotate(Count('blog_posts')).values('id', 'name', 'slug', 'blog_posts__count')
     all_topics = models.Topic.objects.order_by('-blog_posts__count').annotate(Count('blog_posts')).values('name', 'slug')
     all_posts = models.Post.objects.published().order_by('-published')
     return {'authors' : authors, 'latest_posts': latest_posts, 'topics': top_topics[0:10], 'all_posts':all_posts, 'all_topics':all_topics}
--- blog/models.py
+++ blog/models.py
@@ -110,8 +110,6 @@ class Post(models.Model):
                 'year': self.published.year,
                 'month': self.published.month,
                 'day': self.published.day,
-                'slug': self.slug,
-                'banner': self.banner,
             }
         else:
             kwargs = {'pk': self.pk}
